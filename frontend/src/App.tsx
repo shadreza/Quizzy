8b761af938ee49f5bd499d245134ff4e881bbb86
shadreza <shadreza100@gmail.com>
2023-03-20 00:19:09 +0600

    added heroicons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import { BanknotesIcon } from '@heroicons/react/24/solid'
 
 const App = () => {
   return (
-    <div className="bg-white dark:bg-purple-300">App</div>
+    <div className="bg-white dark:bg-purple-300">
+      App
+      <div>
+        <BanknotesIcon className="w-6"/>
+      </div>
+    </div>
   )
 }
 
